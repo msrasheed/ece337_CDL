c4289243c47ed442188f77593676bb61090dde5f
rwpearson333 <rwpearson333@gmail.com>
2019-11-10 14:50:19 -0500

    added RX_Decoder and RX_BitStuffDetector

---
+++ RX_BitStuffDetector.sv
@@ -0,0 +1,76 @@
+//Bit Stuff detector for CDL USB RX
+
+module RX_BitStuffDetector (clk,
+                            n_rst,
+                            decoded_bit,
+                            ignore_bit);
+  input wire clk;
+  input wire n_rst;
+  input wire decoded_bit;
+  output reg ignore_bit;
+
+  typedef enum reg [3:0] {IDLE, COUNT1, COUNT2, COUNT3, COUNT4, COUNT5, COUNT6, SKIP} state_type;
+
+  state_type state;
+  state_type next_state;
+
+  always_comb begin //case statement for state assignment
+    state = IDLE; //prevent latches
+    ignore_bit = 1'b0;
+
+    case(state)
+      IDLE: begin
+        if (decoded_bit == 1'b1) begin
+          next_state = COUNT1;
+        end
+      end
+
+      COUNT1: begin
+        if (decoded_bit == 1'b1) begin
+          next_state = COUNT2;
+        end
+      end
+
+      COUNT2: begin
+        if (decoded_bit == 1'b1) begin
+          next_state = COUNT3;
+        end
+      end
+
+      COUNT3: begin
+        if (decoded_bit == 1'b1) begin
+          next_state = COUNT4;
+        end
+      end
+
+      COUNT4: begin
+        if (decoded_bit == 1'b1) begin
+          next_state = COUNT5;
+        end
+      end
+
+      COUNT5: begin
+        if (decoded_bit == 1'b1) begin
+          next_state = COUNT6;
+        end
+      end
+
+      COUNT6: begin
+        next_state = SKIP;
+      end
+
+      SKIP: begin
+        next_state = IDLE;
+        ignore_bit = 1'b1;
+      end
+    endcase
+  end
+
+  always_ff @(posedge clk, negedge n_rst) begin
+    if (n_rst == 1'b0) begin
+      state <= IDLE;
+    end else begin
+      state <= next_state;
+    end
+  end
+endmodule
---
+++ RX_Decoder.sv
@@ -0,0 +1,55 @@
+//Decoder module for CDL USB RX
+
+module decoder (clk,
+                n_rst,
+                d_plus,
+                d_minus,
+                decoded,
+                eop);
+
+  input wire clk;
+  input wire n_rst;
+  input wire d_plus;
+  input wire d_minus;
+  output reg decoded;
+  output reg eop;
+
+  reg next_decoded;
+  reg next_eop;
+  reg value;
+  reg next_value;
+
+  always_comb begin //comb block for incoming values
+    next_value = value; //Set default value to prevent latches
+    next_eop = 1'b0;
+
+    if (d_plus == 1'b1 && d_minus == 1'b0) begin
+      next_value = 1;
+    end else if (d_plus == 1'b0 && d_minus == 1'b1) begin
+      next_value = 1'b0;
+    end else if (d_plus == 1'b0 and d_minus == 1'b0) begin
+      next_eop = 1'b1;
+    end
+  end
+
+  always_comb begin //comb block for decoding
+    value = 1'b1; //set default to prevent latches
+    if (next_value == value) begin
+      next_decoded = 1'b1;
+    end else if (next_value == value) begin
+      next_decoded = 1'b0;
+    end
+  end
+
+  always_ff @(posedge clk, negedge n_rst) begin
+    if (n_rst == 1'b0) begin
+      value <= 1'b0;
+      decoded <= 1'b0;
+      eop <= 1'b0;
+    end else begin
+      value <= next_value;
+      decoded <= next_decoded;
+      eop <= next_eop;
+    end
+  end
+endmodule

cd33f18d5f8120a940ac12586b1b3a452dad86c7
Melissa Nguyen <mnguye265@gmail.com>
2019-11-17 21:48:04 -0500

    Fixed Data Buffer (11/17: 9:45 PM)
    - Forgot to add data_size to the write_select.
    - Fixed write_select because I accidently mixed up the buffer_reserved.
    - Made the mem [63:0][7:0]
    - Made buffer_occupancy based off of next state (Had to make next state be based off of next state of write and read pointers)
    - Made read_pointer and write_pointer based off of a clock

--- source/data_buffer.sv
+++ source/data_buffer.sv
@@ -40,30 +40,44 @@ module data_buffer
 );
 
 // Initializations
-	reg [7:0] mem [63:0];
-	reg [7:0] next_mem [63:0];
+	reg [63:0][7:0] mem;
+	reg [63:0][7:0] next_mem;
 	reg [5:0] write_pointer;
+	reg [5:0] write_pointer_next;
 	reg [5:0] read_pointer;
-	reg [31:0] read_out;	
+	reg [5:0] read_pointer_next;
+	reg [31:0] rx_data_next;
+	reg [7:0] tx_packet_data_next;
+	reg [6:0] buffer_occupancy_next;
 
 // Sequential Logic for the Write Pointer Counter
 	always_ff @ (posedge clk, negedge n_rst)
 	begin : WRITE_LOGIC
 		if (n_rst == 1'b0)
 			write_pointer <= 6'b0;
-		else begin
-			if (clear == 1'b1)
-				write_pointer <= 6'b0;
-			else if (write_pointer == 6'd63)
-				write_pointer <= write_pointer;
-			else if (store_tx_data == 1'b1 && buffer_reserved == 1'b0)
-				write_pointer <= write_pointer + 1;	// From AHB
-			else if (store_rx_packet_data == 1'b1 && buffer_reserved == 1'b1)
-				write_pointer <= write_pointer + 4;	//From RX
-			else
-				write_pointer <= write_pointer;
-			
+		else
+			write_pointer <= write_pointer_next;
+	end
+
+	always_comb
+	begin : WRITE_LOGIC_NEXT
+		write_pointer_next = write_pointer;
+		if (clear == 1'b1)
+			write_pointer_next = 6'b0;
+		else if (write_pointer == 6'd63)
+			write_pointer_next = write_pointer;
+		else if (store_tx_data == 1'b1 && buffer_reserved == 1'b1) begin
+			case (data_size)
+				2'd0: write_pointer_next = write_pointer + 1;
+				2'd1: write_pointer_next = write_pointer + 2;
+				2'd2: write_pointer_next = write_pointer + 3;
+				2'd3: write_pointer_next = write_pointer + 4;
+				default: write_pointer_next = write_pointer;
+			endcase
 		end
+		else if (store_rx_packet_data == 1'b1 && buffer_reserved == 1'b0)
+			write_pointer_next = write_pointer + 1;	//From RX
+	
 	end
 
 // Sequential Logic for the Read Pointer Counter
@@ -71,19 +85,22 @@ module data_buffer
 	begin : READ_LOGIC
 		if (n_rst == 1'b0)
 			read_pointer <= 6'b0;
-		else begin
-			if (clear == 1'b1)
-				read_pointer <= 6'b0;
-			else if (read_pointer == 6'd63)
-				read_pointer <= read_pointer;
-			else if (get_rx_data == 1'b1)
-				read_pointer <= read_pointer + 4;	// From AHB
-			else if (get_tx_packet_data == 1'b1)
-				read_pointer <= read_pointer + 1;	//From RX
-			else
-				read_pointer <= read_pointer;
+		else
+			read_pointer <= read_pointer_next;
+	end
+
+	always_comb
+	begin : READ_LOGIC_NEXT
+		read_pointer_next = read_pointer;
+		if (clear == 1'b1)
+			read_pointer_next = 6'b0;
+		else if (read_pointer == 6'd63)
+			read_pointer_next <= read_pointer_next;
+		else if (get_rx_data == 1'b1)
+			read_pointer_next = read_pointer + 4;	// From AHB
+		else if (get_tx_packet_data == 1'b1)
+			read_pointer_next = read_pointer + 1;	//From RX
 			
-		end
 	end
 
 // Sequential Logic for the Buffer_Occupancy Output
@@ -92,29 +109,46 @@ module data_buffer
 		if (n_rst == 1'b0)
 			buffer_occupancy <= 7'b0;
 		else
-			buffer_occupancy <= (write_pointer - read_pointer) + 1;		// Does this work out? Should I consider mem[0] as having no data? might work better for the pointer
+			buffer_occupancy <= buffer_occupancy_next;		// Does this work out? Should I consider mem[0] as having no data? might work better for the pointer
+	end
+
+	always_comb
+	begin : BUFFER_OCCUPANCY_NS_LOGIC
+		buffer_occupancy_next = (write_pointer_next - read_pointer_next);
 	end
 
 // Combinational Logic for RX_Data Logic
-	always_comb						// Does this need to be sequential??
+	always_ff @ (posedge clk, negedge n_rst)
 	begin : RX_DATA_AND_TX_DATA_OUTPUT_LOGIC
-		tx_packet_data = 8'b0;
-		rx_data = 32'b0;
+		if (n_rst == 1'b0) begin
+			tx_packet_data <= 8'b0;
+			rx_data <= 32'b0;
+		end
+		else begin
+			tx_packet_data <= tx_packet_data_next;
+			rx_data <= rx_data_next;
+		end
+	end
+
+	always_comb						// Does this need to be sequential??
+	begin : RX_DATA_AND_TX_DATA_NS_LOGIC
+		tx_packet_data_next = 8'b0;
+		rx_data_next = 32'b0;
 		if (get_rx_data == 1'b1) begin
-			rx_data[7:0] = mem[read_pointer];
-			rx_data[15:8] = mem[(read_pointer + 1)];
-			rx_data[23:16] = mem[(read_pointer + 2)];
-			rx_data[31:24] = mem[(read_pointer + 3)];
+			rx_data_next[7:0] = mem[read_pointer];
+			rx_data_next[15:8] = mem[(read_pointer + 1)];
+			rx_data_next[23:16] = mem[(read_pointer + 2)];
+			rx_data_next[31:24] = mem[(read_pointer + 3)];
 		end
 		if (get_tx_packet_data == 1'b1)
-			tx_packet_data = read_out[read_pointer];
+			tx_packet_data_next = mem[read_pointer];
 	end
 
 // Sequential Logic for MEM Block
 	always_ff @ (posedge clk, negedge n_rst)
 	begin : MEM_BLOCK_LOGIC
 		if (n_rst == 1'b0)
-			mem <= '{64{8'b0}};
+			mem <= '0;
 		else
 			mem[63:0] <= next_mem[63:0];
 	end
@@ -123,9 +157,10 @@ module data_buffer
 	always_comb
 	begin : NEXT_MEM_LOGIC
 		next_mem = mem;
-		if (store_rx_packet_data == 1'b1 && buffer_reserved == 1'b1)
+		if (store_rx_packet_data == 1'b1 && buffer_reserved == 1'b0) begin
 			next_mem[write_pointer] = rx_packet_data;
-		else if (store_tx_data == 1'b1 && buffer_reserved == 1'b0) begin
+		end
+		else if (store_tx_data == 1'b1 && buffer_reserved == 1'b1) begin
 			case(data_size)
 				2'd0: next_mem[write_pointer] = tx_data[7:0];
 				2'd1: begin 

a2caea5fe03bd0069cd9203a34509484feae4b50
Moiz Rasheed <mrasheed@purdue.edu>
2019-11-27 16:17:41 -0500

    renamed with tx

--- source/tx_timer.sv
+++ source/tx_timer.sv
@@ -6,10 +6,10 @@ module tx_timer(input wire clk, input wire enable_timer, input disable_timer,  i
 reg shift_strobe_bit_counter;    // count enable from the first counter, resulting in an average speed of 25/3 -> 3 rollovers in 25 clock cycles
 reg [4:0] count_out_ticks;
 
-counter25 CLOCK_COUNT(.clk(clk), .n_rst(n_rst), .count_enable(enable_timer), .clear(clear_timer), .roll_over(shift_strobe_bit_counter), .count_out(count_out_ticks), .flag(flag), .disable_timer(disable_timer));
+tx_counter25 CLOCK_COUNT(.clk(clk), .n_rst(n_rst), .count_enable(enable_timer), .clear(clear_timer), .roll_over(shift_strobe_bit_counter), .count_out(count_out_ticks), .flag(flag), .disable_timer(disable_timer));
 
 // The second value of the NUM_CNT_BITS to be overwritten by data size. Defines how many data bits are to be read
-flex_counter#(
+tx_flex_counter#(
     .NUM_CNT_BITS(4)
     ) BIT_COUNTER( .clk(clk), .n_rst(n_rst), .rollover_val(4'd8), .count_enable(shift_strobe_bit_counter), .clear(clear_timer), .rollover_flag(byte_complete), .count_out(bit_count));
 
--- source/usb_tx.sv
+++ source/usb_tx.sv
@@ -28,20 +28,20 @@ reg clear_timer_fsm;
 reg flag_timer;
 reg clear_crc_fsm;
 
-pts_8_bit SR(.clk(clk), .n_rst(n_rst), .shift_enable(shift_enable_timer && !(disable_bit_stuffer)), .load_enable(load_enable_tx), .parallel_in(parallel_in_tx), .serial_out(serial_out_pts));
+tx_pts_8_bit SR(.clk(clk), .n_rst(n_rst), .shift_enable(shift_enable_timer && !(disable_bit_stuffer)), .load_enable(load_enable_tx), .parallel_in(parallel_in_tx), .serial_out(serial_out_pts));
 
 tx_timer TIM1(.clk(clk) ,.n_rst(n_rst), .shift_strobe(shift_enable_timer),.byte_complete(byte_complete_timer), .enable_timer(enable_timer_fsm), .disable_timer(next_disable_timer_bit_stuffer), .clear_timer(clear_timer_fsm), .flag(flag_timer));
 
-bit_stuffer BST(.clk(shift_enable_timer), .n_rst(n_rst), .serial_in(serial_out_pts), .disable_timer(disable_bit_stuffer), .encoder_in(encoder_in_timer), .next_encoder_in(next_encoder_in_timer), 
+tx_bit_stuffer BST(.clk(shift_enable_timer), .n_rst(n_rst), .serial_in(serial_out_pts), .disable_timer(disable_bit_stuffer), .encoder_in(encoder_in_timer), .next_encoder_in(next_encoder_in_timer), 
                 .next_disable_timer(next_disable_timer_bit_stuffer)); 
 
-crc_16_bit_gen CRC(.clk(clk), .n_rst(n_rst), .serial_in(serial_out_pts), .shift_en(shift_enable_timer && (crc_enable_fsm) && !(disable_bit_stuffer)), .crc(crc_to_fsm), .clear(clear_crc_fsm));
+tx_crc_16_bit_gen CRC(.clk(clk), .n_rst(n_rst), .serial_in(serial_out_pts), .shift_en(shift_enable_timer && (crc_enable_fsm) && !(disable_bit_stuffer)), .crc(crc_to_fsm), .clear(clear_crc_fsm));
 
 tx_fsm FSM(.clk(clk), .n_rst(n_rst), .tx_packet(tx_packet), .state_val(state_val_fsm), .tx_done(tx_done), .crc_enable(crc_enable_fsm), .data_pts(parallel_in_tx), .tx_packet_data(tx_packet_data), 
            .tx_packet_data_size(tx_packet_data_size), .flag(flag_timer), .enable_timer(enable_timer_fsm), .crc(crc_to_fsm), .load_enable(load_enable_tx), .get_tx_packet(get_tx_packet),
            .clear_timer(clear_timer_fsm), .shift_strobe(shift_enable_timer), .clear_crc(clear_crc_fsm));
 
-encoder ENC(.clk(shift_enable_timer), .n_rst(n_rst), .encoder_in(next_encoder_in_timer), .state_val(state_val_fsm), .d_plus(dPlus_out), .d_minus(dMinus_out), .serial_in(serial_out_pts));
+tx_encoder ENC(.clk(shift_enable_timer), .n_rst(n_rst), .encoder_in(next_encoder_in_timer), .state_val(state_val_fsm), .d_plus(dPlus_out), .d_minus(dMinus_out), .serial_in(serial_out_pts));
 
 endmodule
 
